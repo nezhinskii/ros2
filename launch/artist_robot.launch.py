0225c6e831ccdfe03fe7c9de76442789c459f155
nezhinskii <maksimnezhin@gmail.com>
2025-05-20 16:20:35 +0300

    initial urdf

---
+++ launch/artist_robot.launch.py
@@ -0,0 +1,42 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    urdf_file = os.path.join(
+        get_package_share_directory('artist_robot'),
+        'urdf', 'artist_robot.urdf'
+    )
+
+    with open(urdf_file, 'r') as infp:
+        robot_desc = infp.read()
+    
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_desc}]
+        ),
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            output='screen'
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            output='screen'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', os.path.join(get_package_share_directory('artist_robot'), 'config', 'artist_robot.rviz')],
+            output='screen'
+        )
+    ])
