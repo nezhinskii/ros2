b46a9c724379156e38cacc01c7cea5819cbac66e
nezhinskii <maksimnezhin@gmail.com>
2025-05-21 18:20:32 +0300

    added models

---
+++ build/artist_robot/CMakeFiles/artist_robot_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/artist_robot_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/artist_robot_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
